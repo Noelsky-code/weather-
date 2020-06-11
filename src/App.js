e1742a55750f7df6c16f9f1b743b0e84fa72035b
Noelsky-code <hansory@naver.com>
2020-06-11 22:15:44 +0900

    second day

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+state = {
+
+}
+
+componentDidMount(){
+  this._getWeather()
+}
+
+_callApi= () =>{
+  return (fetch('https://api.openweathermap.org/data/2.5/weather?id=6167865&appid=1b35fd0006e39bc93976cade7e656d07')
+          .then(
+            (request)=>{return request.json()}    
+        )
+        .catch((e)=>{console.log(e)})
+  )
+}
+  
+ _getWeather=async()=>{
+   const weather =await this._callApi();
+    this.setState({
+      weather:weather
+    })
+ }
+
+ _renderWeather=()=>{
+    return (<h1>{this.state.weather.name}</h1>
+    );
+    }
+
+  render(){
+    return (
+      <div className = "App">
+      {this.state.weather ? this._renderWeather() : "Loading!"}
+      </div>
+    );
+ }
 }
 
 export default App;
---
+++ src/Weather.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import './Weather.css'
+import './App.js'
+class Weather extends Component{
+
+
+
+
+
+
+}
+
+export default Weather;
